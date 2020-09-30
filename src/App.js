9e29cfcc9422c9c867ccf7cf6ad4e8d1b3bdb783
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-09-30 17:01:27 -0400

    link color

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from './components/Header';
 import Games from './components/Games';
 import Streams from './components/Streams';
 import { Route } from 'react-router-dom';
+import './App.css';
 // import '*bootstrap/dist/css/bootstrap.min.css';
 // import '*shards-ui/dist/css/shards.min.css';
 

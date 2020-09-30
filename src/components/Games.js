817e38451cc689b3c6f267561256e865a4b17edc
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-09-30 16:33:45 -0400

    fetch and store top games

--- src/components/Games.js
+++ src/components/Games.js
@@ -1,9 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { CLIENT_ID, AUTH } from '../apiKey';
 
-const Games = () => (
-  <div>
-    game component
-  </div>
-);
+function Games() {
+  const [games, setGames] = useState([]);
+
+  useEffect(() => {
+    fetch('https://api.twitch.tv/helix/games/top', {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        'Client-ID': CLIENT_ID,
+        'Authorization': `Bearer ${AUTH}`
+      }
+    })
+      .then(res => res.json())
+      .then(data => {
+        setGames(data.data)
+      })
+      .catch(e => console.log)
+
+  }, []);
+
+  return (
+    <div>
+      <h1>Most Popular Games</h1>
+
+    </div>
+  );
+};
 
 export default Games;
